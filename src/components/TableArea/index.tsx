76cac6c54de90512e846bd73c200ff686bec514a
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-25 17:44:30 -0300

    feat: Enhance responsiveness of TableArea and ResumeItem components\n\n- Created  and wrapped the table in  with a responsive container.\n- Created  to ensure  is flexible and adapts to its container.

--- src/components/TableArea/index.tsx
+++ src/components/TableArea/index.tsx
@@ -17,35 +17,37 @@ export const TableArea = ({ list, onDeleteItem }: Props) => {
   };
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th className="table__head-column" style={{ width: 100 }}>
-            Data
-          </th>
-          <th className="table__head-column" style={{ width: 130 }}>
-            Categoria
-          </th>
-          <th className="table__head-column">Título</th>
-          <th className="table__head-column" style={{ width: 150 }}>
-            Valor
-          </th>
-          <th className="table__head-column" style={{ width: 100 }}>
-            Ações
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.length > 0 ? (
-          list.map((item, index) => <TableItem key={index} item={item} onDelete={handleDelete} />)
-        ) : (
+    <div className="table-wrapper">
+      <table className="table">
+        <thead>
           <tr>
-            <td colSpan={5} className="table__empty-message">
-              Nenhuma transação encontrada.
-            </td>
+            <th className="table__head-column" style={{ width: 100 }}>
+              Data
+            </th>
+            <th className="table__head-column" style={{ width: 130 }}>
+              Categoria
+            </th>
+            <th className="table__head-column">Título</th>
+            <th className="table__head-column" style={{ width: 150 }}>
+              Valor
+            </th>
+            <th className="table__head-column" style={{ width: 100 }}>
+              Ações
+            </th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {list.length > 0 ? (
+            list.map((item, index) => <TableItem key={index} item={item} onDelete={handleDelete} />)
+          ) : (
+            <tr>
+              <td colSpan={5} className="table__empty-message">
+                Nenhuma transação encontrada.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
